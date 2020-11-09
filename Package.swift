4807dd163e3c0c735c4eff54bfcae7a7f62a7587
Adir Burke on Laptop <adir@hanave.com>
2020-11-09 16:18:06 +1100

    Fixing to use the library on host

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,6 +9,9 @@ let package = Package(
 		.library(name: "PerfectCZlib", targets: ["PerfectCZlib"])
 	],
 	targets: [
-	.target(name: "PerfectCZlib", path: "PerfectCZlib", exclude: ["contrib", "test", "examples"])
+	.target(name: "PerfectCZlib",
+	      linkerSettings: [
+        		.linkedLibrary("z"),
+      		])
 	]
 )
---
+++ Sources/PerfectCZlib/include/czlib.h
@@ -0,0 +1,49 @@
+//
+//#include "../zconf.h"
+//#include "zlib.h"
+//#include "zutil.h"
+//#include "../inftrees.h"
+#include "zlib.h"
+
+//static inline int CGRPCZlib_deflateInit2(z_streamp stream, int level, int method, int windowBits,
+//                                         int memLevel, int strategy) {
+//  return deflateInit2(stream, level, method, windowBits, memLevel, strategy);
+//}
+//
+//static inline unsigned long CGRPCZlib_deflateBound(z_streamp strm, unsigned long sourceLen) {
+//  return deflateBound(strm, sourceLen);
+//}
+//
+//static inline int CGRPCZlib_deflate(z_streamp strm, int flush) {
+//  return deflate(strm, flush);
+//}
+//
+//static inline int CGRPCZlib_deflateReset(z_streamp strm) {
+//  return deflateReset(strm);
+//}
+//
+//static inline int CGRPCZlib_deflateEnd(z_streamp strm) {
+//  return deflateEnd(strm);
+//}
+//
+//static inline int CGRPCZlib_inflateInit2(z_streamp stream, int windowBits) {
+//  return inflateInit2(stream, windowBits);
+//}
+//
+//static inline int CGRPCZlib_inflate(z_streamp strm, int flush) {
+//  return inflate(strm, flush);
+//}
+//
+//static inline int CGRPCZlib_inflateReset(z_streamp strm) {
+//  return inflateReset(strm);
+//}
+//
+//static inline int CGRPCZlib_inflateEnd(z_streamp strm) {
+//  return inflateEnd(strm);
+//}
+//
+//static inline Bytef *CGRPCZlib_castVoidToBytefPointer(void *in) {
+//  return (Bytef *) in;
+//}
+
+
